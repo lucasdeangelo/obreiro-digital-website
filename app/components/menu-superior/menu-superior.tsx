c9c3fbe72cc5ad5223e290930521d4594b3dd817
Lucas de Angelo <lucasdeangelo343@gmail.com>
2024-01-06 09:34:24 -0300

    Commit Inicial

---
+++ app/components/menu-superior/menu-superior.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '@/public/images/logo.svg'
+import Link from 'next/link'
+
+export default function MenuSuperior() {
+  return (
+    <main>
+      <div className='bg-azul flex py-2 px-14'>
+        <div>
+          <Image src={logo} width={200} height={200} alt=''/>
+        </div>
+        <div className='text-white flex text1 text-2xl justify-center text-center mt-4'>
+          <Link href={''} className='ml-[85dvh] h-8'>Início</Link>
+          <Link href={''} className='ml-[8dvh] h-8'>Missão</Link>
+          <Link href={''} className='ml-[8dvh] h-8'>Recursos</Link>
+          <Link href={''} className='ml-[8dvh] h-8'>Planos</Link>
+          <div className='bg-white text-azul h-[4vh] w-[15vh] flex text-center justify-center rounded-lg ml-[8dvh] text2 text-xl'>
+            <Link href={''} className='mt-1'>Assine Já</Link>            
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'fundo': '#F5F5F5',
+        'azul': '#5271FF',
+        'cinza': '#D9D9D9',
+        'verde': '#30CD36',
+        'amarelo': '#FFCF52',
+        'vermelho': '#FF5252'
+      },
+      boxShadow: {
+        'xl': '0px 0px 22px -5px rgba(0, 0, 0, 0.25)'
+      }
     },
   },
   plugins: [],
