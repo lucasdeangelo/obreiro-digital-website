5845dca2027d9c9300d1e6fbf84607a7ef0d796a
Lucas de Angelo <lucasdeangelo343@gmail.com>
2024-01-07 16:17:07 -0300

    Add - Estilização mobile do Menu Superior

--- app/components/menu-superior/menu-superior.tsx
+++ app/components/menu-superior/menu-superior.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '@/public/images/logo.svg'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import logo from '@/public/images/logo.svg';
+import logotipo from '@/public/images/logotipo-white.svg';
+import hamburguer from '@/public/icons/hamburguer.svg'
+import x from '@/public/icons/x.svg'
+import Link from 'next/link';
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 
 const MenuSuperior: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <main>
-      <div className='bg-azul flex py-2 px-14 fixed top-0 z-50'>
+      {/* Desktop Menu */}
+      <div className='hidden md:flex bg-azul py-2 px-14 fixed top-0 z-50'>
         <div>
           <ScrollLink to="hero" smooth={true} className='cursor-pointer'> 
             <Image src={logo} width={200} height={200} alt=''/>
@@ -23,8 +33,44 @@ const MenuSuperior: React.FC = () => {
           </div>
         </div>
       </div>
+
+      {/* Mobile Menu */}
+      <div className='bg-azul py-2 px-4 md:fixed md:top-0 md:z-50'>
+        <div className='flex justify-between items-center'>
+          <ScrollLink to="hero" smooth={true} className='cursor-pointer'> 
+            <Image src={logotipo} width={60} height={60} alt=''/>
+          </ScrollLink>
+          <button onClick={toggleMenu} className='text-white text-xl focus:outline-none'>
+            {isOpen ? (
+                <Image src={x} width={40} height={40} alt='' />
+              ) : (
+                <Image src={hamburguer} width={50} height={50} alt='' />
+              )}
+          </button>
+        </div>
+
+        {isOpen && (
+          <div className='mt-4 text-white text-left'>
+            <div className='mb-2'>
+              <ScrollLink to="hero" smooth={true} onClick={toggleMenu} className='block cursor-pointer text3 text-2xl'>Início</ScrollLink>
+            </div>
+            <div className='mb-2'>
+              <ScrollLink to="missao" smooth={true} onClick={toggleMenu} className='block cursor-pointer text3 text-2xl'>Missão</ScrollLink>
+            </div>
+            <div className='mb-2'>
+              <ScrollLink to="recursos" smooth={true} onClick={toggleMenu} className='block cursor-pointer text3 text-2xl'>Recursos</ScrollLink>
+            </div>
+            <div className='mb-2'>
+              <ScrollLink to="planos" smooth={true} onClick={toggleMenu} className='block cursor-pointer text3 text-2xl'>Planos</ScrollLink>
+            </div>
+            <div className='bg-white hover:bg-slate-200 text-azul h-[4vh] w-[17vh] flex text-center justify-center rounded-lg text2 text-xl'>
+              <Link href={''} onClick={toggleMenu} className='block cursor-pointer mt-0.5'>Assine Agora</Link>
+            </div>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
 
-export default MenuSuperior
+export default MenuSuperior;
