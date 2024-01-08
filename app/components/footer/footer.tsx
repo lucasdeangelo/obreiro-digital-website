e23557e5130df98e296958e1546fcecca5b67fea
Lucas de Angelo <lucasdeangelo343@gmail.com>
2024-01-07 23:46:36 -0300

    Add - Estilização mobile das sections Social e Footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -5,12 +5,12 @@ import logo from '@/public/images/logo.svg'
 export default function Footer() {
   return (
     <main>
-      <div className='bg-azul flex px-[20vh] py-[2vh]'>
+      <div className='bg-azul flex px-5 py-3 md:px-[20vh] md:py-[2vh]'>
         <div className='flex'>
           <Image src={logo} width={180} height={150} alt=''/>
         </div>
-        <div className='flex items-center ml-[70vh]'>
-          <p className='text2 text-white text-xl'>© Copyright 2023. Ecle. Todos os direitos reservados.</p>
+        <div className='flex items-center ml-[5vh] md:ml-[70vh]'>
+          <p className='text2 text-white md:text-xl'>© Copyright 2023. Ecle. Todos os direitos reservados.</p>
         </div>
       </div>
     </main>
--- app/components/social/social.tsx
+++ app/components/social/social.tsx
@@ -9,8 +9,8 @@ import github from '@/public/icons/github.svg'
 export default function Social() {
   return (
     <main>
-        <div className='flex justify-center self-center py-[15vh]'>
-            <div className="flex bg-azul px-[20vh] py-[3vh] rounded-xl">
+        <div className='flex justify-center self-center pb-[9vh] md:py-[15vh]'>
+            <div className="flex bg-azul px-2 py-4 md:px-[20vh] md:py-[3vh] rounded-xl">
                 <div className='flex justify-center items-center'>
                     <Link href={'https://www.instagram.com/obreirodigital/'}>
                         <Image src={insta} width={40} height={40} alt=''/>
@@ -25,7 +25,7 @@ export default function Social() {
                     </Link>
                 </div>
 
-                <div className='ml-[55vh]'>
+                <div className='ml-5 md:ml-[55vh]'>
                     <h2 className='text1 text-white text-4xl text-right'>Redes Sociais</h2>
                     <p className='text2 text-white text-xl text-right mt-3 leading-6'>Siga-nos nas redes sociais e<br/>acompanhe o nosso projeto!</p>
                 </div>
